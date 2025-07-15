67afb0ee6cdba031a9a08084c72b04e8e40e2476
Saumy1312 <saumysingh60@gmail.com>
2025-07-16 04:11:47 +0530

    added reviews

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 
 app.use(express.urlencoded({extended: true}));
@@ -111,6 +112,19 @@ app.delete("/listings/:id", wrapAsync (async (req,res) =>{
     console.log(deletedListing);
     res.redirect("/listings");
 }));
+
+
+//post route for reviews
+app.post("/listings/:id/reviews", async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();  
+    console.log("Review added:", newReview);
+    res.send("Review added successfully");
+});
 
 
 app.all(/.*/, (req, res, next) => {
